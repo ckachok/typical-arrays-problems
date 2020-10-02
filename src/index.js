260637defdb325bd81e8b8957edd98c9d51ba257
ckachok <mefelion89@gmail.com>
2020-10-03 02:51:10 +0300

    refactor: change code formatting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 
 exports.min = function min (array) {
-return !array || array.length === 0 ? 0 : Math.min.apply(null, array);
+    return !array || array.length === 0 ? 0 : Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-return !array || array.length === 0 ? 0 : Math.max.apply(null, array);
+    return !array || array.length === 0 ? 0 : Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-return !array || array.length === 0 ? 0 : array.reduce((prevValueArray, currValueArray) => 
-(prevValueArray + currValueArray)) / array.length;
-}
-    
+    return !array || array.length === 0 ? 0 : array.reduce((prevValueArray, currValueArray) => 
+                                                           (prevValueArray + currValueArray)) / array.length;
+}    
                                 
